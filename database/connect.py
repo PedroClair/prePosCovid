35677885915d2125f5e12d0a7a56924bb4db276f
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-03-09 08:49:54 -0300

    Adjuste

--- database/connect.py
+++ database/connect.py
@@ -2,10 +2,10 @@ import mysql.connector
 from envPrePosCovid import private as pvt
 
 db = mysql.connector.connect(
-    host=pvt.HOST_MYSQL,
-    user=pvt.USER_MYSQL,
-    password=pvt.PASSWORD_MYSQL,
-    database=pvt.DATABASE_MYSQL
+    host=pvt.HOST,
+    user=pvt.USER,
+    password=pvt.PASSWORD,
+    database=pvt.DATABASE
 ) #Check Connection Settings in MySQL Workbench to get these details
 
 cursor = db.cursor()
