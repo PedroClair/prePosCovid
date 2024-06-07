dbe88c93d8d10d138e3b78b0b7294333e4b942ad
unknown <garcia.pedro.clair@gmail.com>
2024-06-07 16:49:02 -0300

    app with menu

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
 from interface import frontEnd_prePosCovid as FE
 
-FE.questionSemesterBoxPlot(6, '2019-2')
-FE.questionSemester(6)
+def print_menu():
+	print("\n---------------------Welcome--------------------------------")
+	print("1. Generate boxplot question and class: (6, 2019-1)")
+	print("2. Generate boxplot question in all classes: (6)")
+	print("0. Exit")
+	print("---------------------Student Performance------------------------\n")
 
+def main():
+	while True:
+		print_menu()
+		choice = input("Enter your choice: ")
+		if choice == '1':
+			FE.questionSemesterBoxPlot(6, "2019-2")
+			#questionSemesterBoxPlot(6, 2022-1)
+			#questionSemesterBoxPlot(6, 2022-2)
+		elif choice == '2':
+			FE.questionSemester(6)
+		elif choice == '0':
+			print("Exiting ... ")
+			break
+		else:
+			print("Invalid choice. Please try again")
+        
+
+if __name__ == "__main__":
+   main()
+
+#FE.questionSemester(6)
+#FE.questionSemesterBoxPlot(6, "2019-2")
--- interface/frontEnd_prePosCovid.py
+++ interface/frontEnd_prePosCovid.py
@@ -1,18 +1,17 @@
-import service.backEnd_prePosCovid as back
+import service.backEnd_prePosCovid as BE
 import matplotlib.pyplot as plt
 
 def questionSemesterBoxPlot(question, semester):
-    endPoint = back.basicStatistic(question, semester)
-    plt.boxplot(endPoint[0], labels=[semester])
-    plt.title(f"Question: {question}")
-    plt.ylabel('Grades')
-    plt.show()
+	endPoint = BE.basicStatistic(question, semester)
+	plt.boxplot(endPoint[0], labels=[semester])
+	plt.title(f"Question: {question}")
+	plt.ylabel('Grades')
+	plt.show()
 
 def questionSemester(question):
-    list_of_grades, list_of_semesters = back.questionAlongSemesters(question)
-    plt.boxplot(list_of_grades, labels=list_of_semesters)
-    plt.title(f"Question: {question} along semesters")
-    plt.ylabel('Grades')
-    plt.show()
-
+	list_of_grades, list_of_semesters = BE.questionAlongSemesters(question)
+	plt.boxplot(list_of_grades, labels=list_of_semesters)
+	plt.title(f"Question: {question} along semesters")
+	plt.ylabel('Grades')
+	plt.show()
 
