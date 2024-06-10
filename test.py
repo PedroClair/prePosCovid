a2f9b534b41fea15c1be6ad5d5d959a624d6af8c
unknown <garcia.pedro.clair@gmail.com>
2024-06-09 22:34:08 -0300

    boxplot graphs

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ import pandas as pd
 # load the dataset 
 data = sns.load_dataset('tips')
 
-data2 = pd.read_csv('csvToCompareStudentPerformance.csv')
+data2 = pd.read_csv('doc/file/csvToCompareStudentPerformance.csv')
   
 # view the dataset 
-print(data2.head(5))
+print(data2.head(50))
 
 # create grouped boxplot  
 sns.boxplot(x = data2['Question'], 
