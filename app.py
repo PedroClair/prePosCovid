b7266b08ced370f4cc65a13435340c42738fe8e1
unknown <garcia.pedro.clair@gmail.com>
2024-06-13 14:03:19 -0300

    Question alteration

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from interface import frontEnd_prePosCovid as FE
 
 def print_menu():
 	print("\n---------------------Welcome--------------------------------")
-	print("1. Example boxplot question and class: (question=6, class='2019-2')")
-	print("2. Example boxplot question in all classes: (question=6)")
+	print("1. Example boxplot question and class: (question=11, class='2019-2')")
+	print("2. Example boxplot question in all classes: (question=11)")
 	print("3. Figure 2: Common questions analyzed during the semesters in 2019 (prepandemic) and 2022-1 (post-pandemic)")
 	print("0. Exit")
 	print("---------------------Student Performance---------------------\n")
@@ -13,9 +13,9 @@ def main():
 		print_menu()
 		choice = input("Enter your choice: ")
 		if choice == '1':
-			FE.questionSemesterBoxPlot(6, "2019-2")
+			FE.questionSemesterBoxPlot(11, "2019-2")
 		elif choice == '2':
-			FE.questionSemester(6)
+			FE.questionSemester(11)
 		elif choice == '3':
 			FE.showBoxplotComparationPrePosPandemic()
 		elif choice == '0':
